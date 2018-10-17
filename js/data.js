5aa6363abe71270a96458ed09debd96693f7b70b
Teslim Adeyemo <teslimni@gmail.com>
2018-10-17 19:54:41 +0100

    Added some json data manually to simulate fetch request later with the fetch api

---
+++ js/data.js
@@ -0,0 +1,201 @@
+let pages = 
+[
+    {
+        "id": 6,
+        "date": "2018-10-08T22:05:09",
+        "slug": "home",
+        "type": "page",
+        "title": "Home",
+        "content": "Welcome to VanillaPress, my JavaScript site!"
+    },
+    {
+        "id": 7,
+        "date": "2018-10-08T22:05:09",
+        "slug": "about",
+        "type": "page",
+        "title": "About",
+        "content": "A little about me"
+    },
+    {
+        "id": 8,
+        "date": "2018-10-08T22:05:09",
+        "slug": "blog",
+        "type": "page",
+        "title": "Blog",
+        "content": "Please enjoy my posts"
+    },
+    {
+        "id": 6,
+        "date": "2018-10-08T22:05:09",
+        "slug": "contact",
+        "type": "page",
+        "title": "Contact",
+        "content": "Drop me a line with any questions :)"
+    }
+],
+
+courses = 
+[
+    {
+        "id": 1,
+        "date": "2018-10-08T22:05:09",
+        "slug": "sharia-intelligence",
+        "type": "course",
+        "title": "Sharia Intelligence",
+        "content": "A course about Sharia Intelligence",
+        "status": "released"
+    },
+    {
+        "id": 2,
+        "date": "2018-10-08T22:05:09",
+        "slug": "islam-gender-equity",
+        "type": "course",
+        "title": "Islam and Gender Equity",
+        "content": "A course about Islam and Gender Equity",
+        "status": "released"
+    },
+    {
+        "id": 3,
+        "date": "2018-10-08T22:05:09",
+        "slug": "interfaith-relations",
+        "type": "course",
+        "title": "Interfaith Relations",
+        "content": "A course about Interfaith Relations",
+        "status": "released"
+    },
+    {
+        "id": 4,
+        "date": "2018-10-08T22:05:09",
+        "slug": "intrafaith-relations",
+        "type": "course",
+        "title": "Intrafaith Relations",
+        "content": "A course about Intrafaith Relations",
+        "status": "released"
+    },
+    {
+        "id": 5,
+        "date": "2018-10-08T22:05:09",
+        "slug": "jihad",
+        "type": "course",
+        "title": "Jihad",
+        "content": "A course about Jihad",
+        "status": "released"
+    },
+    {
+        "id": 6,
+        "date": "2018-10-08T22:05:09",
+        "slug": "misconceptions-islam",
+        "type": "course",
+        "title": "Misconceptions about Islam",
+        "content": "A course about Misconceptions about Islam",
+        "status": "released"
+    },
+    {
+        "id": 7,
+        "date": "2018-10-08T22:05:09",
+        "slug": "questions-god",
+        "type": "course",
+        "title": "Questions about God",
+        "content": "A course about Questions about God",
+        "status": "released"
+    },
+    {
+        "id": 8,
+        "date": "2018-10-08T22:05:09",
+        "slug": "islamic-jurisprudence",
+        "type": "course",
+        "title": "Islamic Jurisprudence",
+        "content": "A course about Islamic Jurisprudence",
+        "status": "released"
+    },
+    {
+        "id": 9,
+        "date": "2018-10-08T22:05:09",
+        "slug": "islam-polygamy",
+        "type": "course",
+        "title": "Islam and Polygamy",
+        "content": "A course about Islam and Polygamy",
+        "status": "coming soon"
+    },
+    {
+        "id": 10,
+        "date": "2018-10-08T22:05:09",
+        "slug": "islam-non-muslims",
+        "type": "course",
+        "title": "Ethics of Disagreement in Islam",
+        "content": "A course about Islam and Non-Muslims",
+        "status": "coming soon"
+    },
+    {
+        "id": 11,
+        "date": "2018-10-08T22:05:09",
+        "slug": "ethics-disagreement",
+        "type": "course",
+        "title": "Ethics of Disagreement in Islam",
+        "content": "A course about Ethics of Disagreement in Islam",
+        "status": "coming soon"
+    }
+],
+
+users = 
+[
+    {
+        "id": 1,
+        "name": "Teslim Adeyemo",
+        "role": "tutor",
+        "description": "Full Process Digital and Mobile Web Developer",
+        "course": "Digital Strategy",
+    },
+    {
+        "id": 2,
+        "name": "Ismail Abdul Kadir",
+        "role": "tutor",
+        "description": "Shariah Intelligence Specialist",
+        "course": "Shariah Intelligence",
+    },
+    {
+        "id": 3,
+        "name": "Attahir Baba Mina",
+        "role": "tutor",
+        "description": "Relations with Non-Muslims Specialist",
+        "course": "Relations with Non-Muslims",
+    },
+    {
+        "id": 4,
+        "name": "Nuru Lemu",
+        "role": "tutor",
+        "description": "PVE-TAN Specialist",
+        "course": "PVE-TAN",
+    },
+    {
+        "id": 5,
+        "name": "Jimoh Ibrahim",
+        "role": "tutor",
+        "description": "Islam, Gneder and Equity Specialist",
+        "course": "Islam, Gneder and Equity",
+    },
+    {
+        "id": 6,
+        "name": "Haleemah Ahmad",
+        "role": "tutor",
+        "description": "Misconceptions about Islam Specialist",
+        "course": "Misconceptions about Islam",
+    },
+    {
+        "id": 7,
+        "name": "Ibrahim Bello",
+        "role": "tutor",
+        "description": "Questions about God Specialist",
+        "course": "Questions about God",
+    },
+    {
+        "id": 8,
+        "name": "Yahya D",
+        "role": "tutor",
+        "description": "Islamic and Jurisprudence Specialist",
+        "course": "Islamic and Jurisprudence",
+    }
+
+],
+
+data = {"pages": pages, "courses": courses, "users": users};
